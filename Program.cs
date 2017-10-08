fd69be5d95e8467b3c88cb6f62db03c23e7524cf
Adrien M <hadolf@gmail.com>
2017-10-08 18:57:36 +0200

    opening api to outside

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace IpfsUploader
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
